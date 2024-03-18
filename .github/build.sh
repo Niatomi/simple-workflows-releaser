a9c03961540979096ca6c40bcfef60fbbdcd999f
Niatomi <playervoker@gmail.com>
2024-03-18 16:30:53 +0400

    Restructured

---
+++ .github/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+### Helper functions ###
+
+function header() {
+    echo "\033[1;34m$1\033[0m"
+}
+
+function info() {
+    echo "\033[0;32m$1\033[0m"
+}
+
+function debug() {
+    echo "\033[0;33m$1\033[0m"
+}
+
+function success() {
+    echo "\033[0;32m$1\033[0m"
+}
+
+function error() {
+    echo "\033[0;31m$1\033[0m"
+}
+
+# Header
+
+header "Application Builder"
+header "======================"
+
+
+cwd=$(pwd)
+
+info "Build Context: $cwd"
+
+info "Removing old binaries"
+
+rm -rf $cwd/dist
+
+success "Removed old binaries"
+
+# Options
+
+AppVersion=""
+OutDir=$cwd/dist
+
+if [ -n "$VERSION" ]; then
+    AppVersion="$VERSION"
+fi
+
+debug "App Version: $AppVersion"
+debug "Out Dir: $OutDir"
+
+# Build
+
+AppSuffix=""
+
+if [[ AppVersion != "" ]]; then
+    AppSuffix="-v$AppVersion"
+fi
+
+mkdir -p $OutDir
+
+touch hello.txt
+
+success "Bundle built"
+
+cd $cwd
